105a5e6435a1f9a1faaa47d18110a7c7237c8e74
Christin Reiners <reiners.christian@web.de>
2020-02-11 08:12:54 +0100

    initial commit

---
+++ xslupdate.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 07:15:56 2020
+
+@author: Christian.Reiners
+"""
+
+import win32com.client
+
+# Start an instance of Excel
+xlapp = win32com.client.DispatchEx("Excel.Application")
+
+# Open the workbook in said instance of Excel
+wb = xlapp.workbooks.open("U:\PMO-Übersicht.xlsm")
+
+# Optional to see the instance
+# xlapp.Visible = True
+
+# Refresh all data connections.
+wb.RefreshAll()
+wb.Save()
+
+# Quit
+xlapp.Quit()
